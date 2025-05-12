363c89e63c4b1c2a545b54f9cbc16f31add49c2b
ankit.verma <admin@SIMPL-X7QVQGGFPD.local>
2025-05-12 14:08:22 +0530

    add get student by id

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"strconv"
 
 	"github.com/ankit8125/crud-golang-practice/internal/storage"
 	"github.com/ankit8125/crud-golang-practice/internal/types"
@@ -14,11 +15,11 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-func New(storage storage.Storage ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request){
-		
+func New(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
 		var student types.Student
-		
+
 		slog.Info("creating a student")
 
 		err := json.NewDecoder(r.Body).Decode(&student)
@@ -33,12 +34,12 @@ func New(storage storage.Storage ) http.HandlerFunc {
 		}
 
 		// Request validation
-		if err:= validator.New().Struct(student); err != nil {
+		if err := validator.New().Struct(student); err != nil {
 			validateErrs := err.(validator.ValidationErrors)
 			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrs))
 			return
 		}
-		
+
 		lastId, err := storage.CreateStudent(
 			student.Name,
 			student.Email,
@@ -53,5 +54,26 @@ func New(storage storage.Storage ) http.HandlerFunc {
 
 		response.WriteJson(w, http.StatusCreated, map[string]int64{"id": lastId})
 		// w.Write([]byte("welcome to students api"))
+	}
+}
+
+func GetById(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request){
+		id := r.PathValue("id")
+		slog.Info("getting a student", slog.String("id", id))
+		
+		intId, err := strconv.ParseInt(id, 10, 64)
+		if err != nil{
+			 response.WriteJson(w, http.StatusBadRequest, response.GeneralError((err)))
+		}
+		student, err := storage.GetStudentById(intId)
+
+		if err != nil {
+			slog.Error("error getting user", slog.String("id", id))
+			response.WriteJson(w, http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+
+		response.WriteJson(w, http.StatusOK, student)
 	}
 }
--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -2,8 +2,11 @@ package sqlite
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/ankit8125/crud-golang-practice/internal/config"
+	"github.com/ankit8125/crud-golang-practice/internal/types"
+
 	// "github.com/ankit8125/crud-golang-practice/internal/storage"
 	_ "github.com/mattn/go-sqlite3" // This is being used indirectly, that's why we use '_'
 )
@@ -56,4 +59,23 @@ func (s *Sqlite) CreateStudent(name string, email string, age int) (int64, error
 	}
 
 	return lastId, nil
+}
+
+func (s *Sqlite) GetStudentById(id int64) (types.Student, error){
+	stmt, err := s.Db.Prepare("SELECT * FROM students WHERE Id = ? LIMIT 1")
+	if err != nil {
+		return types.Student{}, err
+	}
+	defer stmt.Close()
+
+	var student types.Student
+
+	stmt.QueryRow(id).Scan(&student.Id, &student.Name, &student.Email, &student.Age)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return types.Student{}, fmt.Errorf("no student found with id %s", fmt.Sprint(id))
+		}
+		return types.Student{}, fmt.Errorf("query error: %w", err)
+	}
+	return student, nil
 }
